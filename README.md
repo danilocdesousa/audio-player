84e527fd3f90225c6bb343ad258e91c4b60a9fed
Danilo <45634291+danilocdesousa@users.noreply.github.com>
2022-04-07 16:47:55 -0300

    Iniciar a estrutura do layout

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# audio-player
+# Trabalhando com HTML Áudio, CSS e JavaScript
+
+**Este projeto é um estudo baseado no conteúdo do canal da Rocketseat no YouTube.**
+
+<img src="assets/screenshots/screenshot-127.0.0.1_5500-2022.04.05-15_31_08.png">
+
+## Audio Player
+Ao longo do vídeo é explicado como usar a tag de áudio HTML5, que permite criar um reprodutor de áudio, utilizando Javascript puro e o Materialize como um framework CSS.
+
+Para mais informações, veja [Criando Player de Áudio com Javascript | Mayk Brito](https://www.youtube.com/watch?v=vqrjFnq3-uo).
---
+++ assets/scripts/data.js
@@ -0,0 +1,26 @@
+window.audios = [
+    {
+        title: "Ambient Cinematic Hip-Hop",
+        artist: "Anton Vlasov",
+        cover: "hand-1850120_640.jpg",
+        file: "into-the-night-20928"
+    },
+    {
+        title: "Order",
+        artist: "ComaStudio",
+        cover: "woman-6194370_640.jpg",
+        file: "order-99518.mp3"
+    },
+    {
+        title: "Price of Freedom",
+        artist: "ZakharValaha",
+        cover: "piano-1835179_640.jpg",
+        file: "price-of-freedom-33106.mp3"
+    },
+    {
+        title: "Showreel",
+        artist: "QubeSounds",
+        cover: "pioneer-5450710_640.jpg",
+        file: "showreel-99195.mp3"
+    }
+];
