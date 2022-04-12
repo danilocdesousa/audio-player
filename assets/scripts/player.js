82322daa68d7602a3aca2e7f9314c019d046017f
Danilo <45634291+danilocdesousa@users.noreply.github.com>
2022-04-12 18:38:44 -0300

    Retornar ao primeiro áudio após o último

--- assets/scripts/player.js
+++ assets/scripts/player.js
@@ -4,34 +4,38 @@ window.player = {
     artist: document.querySelector(".artist"),
     cover: document.querySelector(".card-image"),
     audio: document.querySelector("audio"),
-
     // Dados dos áudios.
     audioData: audioList,
     currentAudio: {},
     currentPlaying: 0,
-
     // Configurações do player.
     start() {
         this.update();
-
         // Evento para passar para o próximo item da lista quando o atual estiver terminado.
         this.audio.onended = () => this.next();
     },
-
     next() {
         this.currentPlaying++;
+        // Testa se o áudio atual é o último da lsita.
+        if (this.currentPlaying == this.audioData.length) {
+            this.restart();
+        }
         this.update();
+        // Coloca o próximo áudio em estado de play.
         this.audio.play();
     },
-
     update() {
         // Setar os dados na lista de audios.
         this.currentAudio = this.audioData[this.currentPlaying];
-
         // Alteração dos dados nos elementos HTML.
         this.title.innerText = this.currentAudio.title;
         this.artist.innerText = this.currentAudio.artist;
         this.cover.style.background = `url('${path(player.currentAudio.cover)}') no-repeat center center / cover`;
         this.audio.src = path(this.currentAudio.audio);
+    },
+    restart() {
+        // Depois de terminar de tocar o último áudio, volte para o primeiro áudio.
+        this.currentPlaying = 0;
+        this.update();
     }
 };
